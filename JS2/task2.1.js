1eb4c2413c9b3173595291cee2f2e652b7bb5d0b
Nicolevik <nicole.vik@mail.ru>
2016-08-10 16:42:42 +0300

    новое

---
+++ JS2/task2.1.js
@@ -0,0 +1,22 @@
+var login = prompt ('Введите ваш Login', '');
+var password = 'password'
+if (login == 'admin') {
+    alert ('Введите пароль', '');
+} else if (password = 'password') {
+    alert('Welcome home!');
+}
+else {
+    alert ('Wrong password');
+}
+
+
+
+
+
+} else if (login == '') {
+    alert();
+} else {
+    alert();
+}
+
+
---
+++ JS2/task2.3.js
@@ -0,0 +1,11 @@
+var name = 'admin', text;
+
+if (name == 'admin') {
+    text = 'Hi';
+} else if (name == 'manager') {
+    text = 'Hello';
+} else if (name == '') {
+    text = 'No login';
+} else {
+    text = '';
+}
